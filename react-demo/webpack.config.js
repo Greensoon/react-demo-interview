16531992f975416c3f52bda61d4b3f4b1e585dfa
Greenicb <xiaoyaozhen3930@163.com>
2016-03-22 20:12:13 +0800

    add dir build

--- react-demo/webpack.config.js
+++ react-demo/webpack.config.js
@@ -4,3 +4,25 @@
 var webpack = require('webpack');
 
 var path = require("path");
+var srcPath = path.join(__dirname,"src");
+
+module.exports={
+    entry : path.join(srcPath,"entry.js"),
+    output : {
+        filename : "bundle.min.js",
+        path     : "./build"
+    },
+    module : {
+        loaders :[
+            {test : /\.js?$/,exclude:/node_modules/,loader:"babel"},
+            {
+                test : /\.less/,loader : "style!css!less"
+            }
+        ]
+    },
+
+    devServer : {
+        port : "3000",
+        contentBase : "./build"
+    }
+}
